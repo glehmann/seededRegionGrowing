26c4055e90cc17c794d3982be59da7ab178c8700
richardb <none>
2007-01-03 06:56:49 +0100

    finished restructure
    darcs-hash:20070103055649-fafb9-2c17da6eb6334650ce68a9fefaa362dfa9091dd3.gz

---
+++ itkSeededRegionGrowingSupport.h
@@ -0,0 +1,58 @@
+#ifndef __itk_SeededRegionGrowingSupport_h
+#define __itk_SeededRegionGrowingSupport_h
+
+#include <map>
+#include "itkNumericTraits.h"
+
+namespace itk {
+
+template <class LabType, class PixType>
+class SRGRegionStatsType
+{
+public:
+  typedef typename NumericTraits<PixType>::RealType RealType;
+
+  RealType computePriority(LabType region,
+			   PixType  candidate)
+  {
+    typename StatsMapType::const_iterator reg = m_StatsMap.find(region);
+    RealType regmean = reg->second.m_Sum/((RealType)reg->second.m_Count);
+    return (RealType)fabs(regmean - (RealType)candidate);
+  }
+
+  void updateRegion(LabType region,
+		    PixType candidate)
+  {
+    typename StatsMapType::iterator reg = m_StatsMap.find(region);
+    ++(reg->second.m_Count);
+    reg->second.m_Sum += (RealType)candidate;
+  }
+
+  void initRegion(LabType region,
+		  PixType candidate)
+  {
+    ++(m_StatsMap[region].m_Count);
+    m_StatsMap[region].m_Sum += (RealType)candidate;
+  }
+
+private:
+  typedef class RegionStatisticsType
+  {
+  public:
+    RegionStatisticsType()
+    {
+      m_Count = 0;
+      m_Sum = NumericTraits<RealType>::Zero;
+    }
+    
+    RealType m_Sum;
+    unsigned long m_Count;
+  };
+
+  typedef typename std::map<LabType, RegionStatisticsType> StatsMapType;
+
+  StatsMapType m_StatsMap;
+};
+
+}
+#endif
