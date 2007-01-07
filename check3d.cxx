9d2c9b03fcc28e884d59ddbce50f5ac8930cdcfe
richardb <none>
2007-01-07 22:55:03 +0100

    added 3d test
    darcs-hash:20070107215503-fafb9-8a8f59933bc7b492d04033b0c09f80a99dac97d8.gz

---
+++ check3d.cxx
@@ -0,0 +1,47 @@
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkCommand.h"
+#include "itkSimpleFilterWatcher.h"
+
+#include "itkSeededRegionGrowingImageFilter.h"
+
+
+int main(int argc, char * argv[])
+{
+
+  if( argc !=  6)
+    {
+    std::cerr << "usage: " << argv[0] << " " << std::endl;
+    // std::cerr << "  : " << std::endl;
+    exit(1);
+    }
+
+  const int dim = 3;
+  bool connect = atoi(argv[4]);
+  bool line = atoi(argv[5]);
+  typedef unsigned short PType;
+  typedef itk::Image< PType, dim > IType;
+
+  typedef itk::ImageFileReader< IType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+  ReaderType::Pointer sreader = ReaderType::New();
+  sreader->SetFileName( argv[2] );
+
+  typedef itk::SeededRegionGrowingImageFilter< IType, IType > FilterType;
+  FilterType::Pointer filter = FilterType::New();
+  filter->SetInput( reader->GetOutput() );
+  filter->SetMarkerImage( sreader->GetOutput() );
+  filter->SetFullyConnected(connect);
+  filter->SetMarkBoundaryLine(line);
+  itk::SimpleFilterWatcher watcher(filter, "filter");
+
+  typedef itk::ImageFileWriter< IType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( filter->GetOutput() );
+  writer->SetFileName( argv[3] );
+  writer->Update();
+
+  return 0;
+}
+
